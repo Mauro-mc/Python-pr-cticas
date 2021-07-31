7ecbb6514ad287d9d09a30d4a3dce4ae7b8946f4
Mauricio <88127400+Mauro-mc@users.noreply.github.com>
2021-07-30 19:16:06 -0500

    Add files via upload

---
+++ 03_personal-info.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 30 18:44:57 2021
+
+@author: Usuario
+"""
+
+nombre=input("Ingrese su nombre:")
+apellido= input("Ingrese su apellido:")
+ubicacion=input ("Ingrese su ubicación:")
+edad= input ("Ingrese su edad:")
+
+espacio= " "
+
+print("Hola",espacio, nombre,espacio,apellido,espacio,"su ubicación actual es:",espacio,ubicacion,espacio,"y su edad es:",espacio,edad)
