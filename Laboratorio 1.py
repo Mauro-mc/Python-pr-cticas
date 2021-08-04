7f2253bcd4ae5d3d662b1cef99f41e443f50aeef
Mauricio <88127400+Mauro-mc@users.noreply.github.com>
2021-08-03 22:07:16 -0500

    Add files via upload

---
+++ Laboratorio 1.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug  1 13:00:54 2021
+
+@author: Mauricio Carrión
+"""
+# 3x^3-2x^2+3x-1
+
+x=float(input("Ingrese el valor de x: "))
+
+y = ((3*x**3)-(2*x**2)+(3*x)-1)
+
+print ("El valor de 'y' es: ", y)
+
+
+#Muestra
+# x=0
+# x=1
+# x=-1
+
+#Salida 
+# y= -1.0
+# y = 3.0
+# y = -9.0
